ad21f6663515214f2881854e79e97408f128cb6c
mira <gom21194@gmail.com>
2020-08-13 19:29:39 +0900

    login

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Button, Progress } from 'semantic-ui-react'
+import {Button, Input} from 'semantic-ui-react'
+
+function Login(){
+
+return(
+  <div>
+  <Input/>
+  <Button onClick = { () => alert("버튼이 눌림!") } > 로그인!1 </Button>
+  </div>
+
+  )
+}
+
 
 function App() {
   return (
     <div>
 <p> 안녕하세요</p>
-<Button primary> 하이하이1 </Button>
-<Progress percent = {66} indicating/>    
+<button> 하이하이 </button>
+<Button> 하이하이1 </Button>
+<Login/>
     </div>
   );
 }
