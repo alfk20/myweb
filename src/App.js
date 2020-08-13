42c25fe9f0f07348a02026707421df8717146079
mira <gom21194@gmail.com>
2020-08-13 20:10:54 +0900

    move

--- src/App.js
+++ src/App.js
@@ -1,47 +1,14 @@
 import React from 'react';
 import {Button, Input} from 'semantic-ui-react'
 
-function Login2(){
-
-return(
-  <div>
-  <Input/>
-  <Button onClick = { (e) => {
-    console.log(e)
-    alert(e.target.value)
-  } } > log in! </Button>
-  </div>
-
-  )
-}
-
-class Login extends React.Component{
-constructor(){
-  super()
-  this.state = {id : "jashfha"}
-}
-
-  render(){
-    return(
-      <div>
-      <Input onChange = { (e) => this.setState({id : e.target.value }) }/>
-      <Button onClick = { (event) => {
-        alert(this.target.id)
-      } } > Login </Button>
-      </div>
-    )
-  }
-}
+import space from "./space.jpeg"
 
 
 function App() {
   return (
     <div>
-<p> 안녕하세요</p>
-<button> 하이하이 </button>
-<Button> 하이하이1 </Button>
-<Login/>
-    </div>
+  <img src = {space}/>
+  </div>
   );
 }
 
