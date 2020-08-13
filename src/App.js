f6a92d7e987b505954d8da6eed977fb0e2c74e44
mira <gom21194@gmail.com>
2020-08-13 20:43:04 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,39 +1,9 @@
 import React from 'react';
-import {Image, Button, Comment, Form, Header} from 'semantic-ui-react'
+import {Image} from 'semantic-ui-react'
+
+import Comments from './comments.js'
 
 import space from "./space.jpeg"
-import anon from "./anon-icon.png"
-
-class Comments extends React.Component{
-
-  render(){
-    return(<Comment.Group>
-       <Header as='h3' dividing>
-         Comments
-       </Header>
-
-       <Comment>
-         <Comment.Avatar src={anon} />
-         <Comment.Content>
-           <Comment.Author as='a'>Alien</Comment.Author>
-           <Comment.Metadata>
-             <div>Today at 5:42PM</div>
-           </Comment.Metadata>
-           <Comment.Text>How artistic!</Comment.Text>
-           <Comment.Actions>
-             <Comment.Action>Reply</Comment.Action>
-           </Comment.Actions>
-         </Comment.Content>
-       </Comment>
-
-       <Form reply>
-         <Form.TextArea />
-         <Button content='Add Reply' labelPosition='left' icon='edit' primary />
-       </Form>
-     </Comment.Group>
-    )
-  }
-}
 
 function App() {
   return (
