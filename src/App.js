219c7e5b9031132043d71e672f4c7b25d8e3bb7d
mira <gom21194@gmail.com>
2020-08-20 19:22:51 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Button, Image, Divider, Header, Icon} from 'semantic-ui-react'
+import {Button, Image, Divider, Header, Icon, Grid} from 'semantic-ui-react'
 
 import Comments from './comments.js'
 
@@ -9,27 +9,30 @@ function App() {
   return (
     <div>
   <Image src = {space} centered size='large' />
-
+  <div>
+    <Button
+      content='Like'
+      icon='heart'
+      label={{ as: 'a', basic: true, content: '2,048' }}
+      labelPosition='right'
+    />
+  </div>
   <Divider horizontal>
 <Header as='h4'>
   <Icon name='comment alternate outline' />
   cmnts
 </Header>
 </Divider>
+
   <Comments />
-  <div>
-  <Button color='facebook'>
-    <Icon name='facebook' /> Facebook
-  </Button>
-  <Button color='twitter'>
-    <Icon name='twitter' /> Twitter
-  </Button>
-  <Button color='instagram'>
-    <Icon name='instagram' /> Instagram
-  </Button>
-  <Button color='youtube'>
-    <Icon name='youtube' /> YouTube
-  </Button>
+  <br/>
+<div>
+<Grid centered>
+<Button circular color='facebook' icon='facebook' />
+<Button circular color='twitter' icon='twitter' />
+<Button circular color='instagram' icon='instagram' />
+<Button circular color='youtube' icon='youtube' />
+</Grid>
 </div>
   </div>
   );
