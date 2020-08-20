7bb813e50bbb8040c74049c963671f9185d44591
mira <gom21194@gmail.com>
2020-08-20 23:41:29 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,138 @@
-import React from 'react';
-import {Button, Image, Divider, Header, Icon, Grid} from 'semantic-ui-react'
+import React from "react";
+import firebase from "firebase";
+import {
+  Button,
+  Image,
+  Divider,
+  Header,
+  Icon,
+  Grid,
+  Input,
+  Menu, Segment
+} from "semantic-ui-react";
 
-import Comments from './comments.js'
+import Comments from "./comments.js";
+import Buttons from "./buttons.js";
+import auth from "./fb.js";
 
-import space from "./space.jpeg"
+import space from "./space.jpeg";
 
-function App() {
-  return (
-    <div>
-  <Image src = {space} centered size='large' />
-  <div>
-    <Button
-      content='Like'
-      icon='heart'
-      label={{ as: 'a', basic: true, content: '2,048' }}
-      labelPosition='right'
-    />
-  </div>
-  <Divider horizontal>
-<Header as='h4'>
-  <Icon name='comment alternate outline' />
-  cmnts
-</Header>
-</Divider>
+var provider = new firebase.auth.GoogleAuthProvider();
 
-  <Comments />
-  <br/>
-<div>
-<Grid centered>
-<Button circular color='facebook' icon='facebook' />
-<Button circular color='twitter' icon='twitter' />
-<Button circular color='instagram' icon='instagram' />
-<Button circular color='youtube' icon='youtube' />
-</Grid>
-</div>
-  </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      userName : "visitor"
+    }
+  }
+
+state = { activeItem: 'home'}
+handleItemClick = (e, { name }) => this.setState({ activeItem: name})
+
+  render() {
+    const { activeItem } = this.state
+
+    return (
+      <div style={{ backgroundColor: "SlateGray" }}>
+        <h2 style={{ textAlign: "center", paddingTop: "10px" }}>
+        {" "}
+        GOMWEB {" "}
+        </h2>
+
+        <Segment style={{ backgroundColor: "LightSlateGray" }}>
+        <Menu pointing secondary widths={3}>
+          <Menu.Item
+          active={activeItem === 'home'}
+          onClick={this.handleItemClick}
+          >
+          home </Menu.Item>
+          <Menu.Item> {`반가워요,  ${this.state.userName}님.`} </Menu.Item>
+          <Menu.Item
+          active={activeItem === 'login'}
+          onClick={() => {
+             firebase
+               .auth()
+               .signInWithPopup(provider)
+               .then(function (result) {
+                 // This gives you a Google Access Token. You can use it to access the Google API.
+                 var token = result.credential.accessToken;
+                 // The signed-in user info.
+                 var user = result.user;
+                 return user.displayName;
+                 // ...
+               })
+               .then((result) => this.setState({ userName: result }))
+               .catch(function (error) {
+                 // Handle Errors here.
+                 var errorCode = error.code;
+                 var errorMessage = error.message;
+                 // The email of the user's account used.
+                 var email = error.email;
+                 // The firebase.auth.AuthCredential type that was used.
+                 var credential = error.credential;
+                 // ...
+               });
+           }}
+          >
+          login
+          </Menu.Item>
+        </Menu>
+        </Segment>
+
+        <Grid centered>
+          <Grid.Row>
+            <Image src={space} centered size="large" />
+          </Grid.Row>
+          <Grid.Row>
+            <Buttons />
+          </Grid.Row>
+        </Grid>
+        <br />
+
+        <Divider horizontal>
+          <Header as="h4">
+          </Header>
+        </Divider>
+
+        <Comments/>
+
+        <div>
+            <Button
+              circular
+              color="facebook"
+              icon="facebook"
+              floated='right'
+              onClick={() => alert("준비중")}
+            />
+            <Button
+              circular
+              color="twitter"
+              icon="twitter"
+              floated='right'
+              onClick={() => window.open("https://www.twitter.com/")}
+            />
+            <Button
+              circular
+              color="instagram"
+              icon="instagram"
+              floated='right'
+              onClick={() => window.open("https://www.instagram.com/")}
+            />
+            <Button
+              circular
+              color="youtube"
+              icon="youtube"
+              floated='right'
+              onClick={() => window.open("https://www.youtube.com/")}
+            />
+        </div>
+        <br/>
+        <br/>
+        <br/>
+      </div>
+    );
+  }
 }
 
 export default App;
