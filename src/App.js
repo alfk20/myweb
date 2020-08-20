a2dec5482e9df0975b4f6777a15dcb14107c614a
mira <gom21194@gmail.com>
2020-08-20 18:55:05 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Image} from 'semantic-ui-react'
+import {Image, Divider, Header, Icon} from 'semantic-ui-react'
 
 import Comments from './comments.js'
 
@@ -9,6 +9,13 @@ function App() {
   return (
     <div>
   <Image src = {space} centered size='large' />
+
+  <Divider horizontal>
+<Header as='h4'>
+  <Icon name='tag' />
+  Description
+</Header>
+</Divider>
   <Comments />
   </div>
   );
